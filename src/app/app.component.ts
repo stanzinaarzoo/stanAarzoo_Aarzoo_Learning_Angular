4ba79dff9f9c51e3fa18a145f96ebd901efb029e
Aarzoo <w0864444@myscc.ca>
2024-09-18 10:13:37 -0400

    assignment 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Product } from './Shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,51 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  Products: Product[] = [
+    {
+      id: 1,
+      productName: "Desktop",
+      company: "Dell",
+      vendor: "js tech sol",
+      price: 1000,
+      inStock: false
+    },
+    {
+      id: 2,
+      productName: "iphone 11",
+      company: "apple",
+      vendor: "Mobile LLP",
+      price: 1100,
+      inStock: false
+    },
+    {
+      id: 3,
+      productName: "Airpods",
+      company: "apple",
+      vendor: "SoundZone",
+      price: 130,
+      inStock: true
+    },
+    {
+      id: 4,
+      productName: "Monitor",
+      company: "LG",
+      vendor: "led and sons",
+      price: 100,
+      inStock: false
+    },
+    {
+      id: 5,
+      productName: "Mouse",
+      company: "dell",
+      vendor: "BBT",
+      price: 10,
+      inStock: true
+    }
+  ];
+
+
   name = 'Aarzoo';
   age = 23;
+  theme = "ecom";
 }
