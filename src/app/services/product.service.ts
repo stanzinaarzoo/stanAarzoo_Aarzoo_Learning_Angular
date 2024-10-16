0779fb851945407fce5734a475c10315ac07e6d7
Aarzoo <w0864444@myscc.ca>
2024-10-16 15:51:45 -0400

    finish assignment step 9

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -11,7 +11,36 @@ export class ProductService {
   constructor() { }
 
   getProducts() : Observable<Product[]> {
-    return of(this.products);
 
+    return of(this.products);
   }
+    //Adding basic CRUD methods
+    //Create: Add Product
+    addProducts(newProduct:Product) : Observable<Product []> {
+      this.products.push(newProduct)
+      return of(this.products);
+    }
+
+    // Update an existing user
+    updateProduct(updatedProduct: Product): Observable<Product[]> {
+      const index = this.products.findIndex(product => product.id === updatedProduct.id);
+      if (index !== -1) {
+        this.products[index] = updatedProduct;
+    }
+      return of(this.products);
+    }
+    // Delete: Remove a user by ID
+    deleteProduct(productId: number) : Observable<Product[]> {
+      this.products = this.products.filter(product => product.id !== productId);
+      return of(this.products);
+    }
+    getProductById(productId: number): Observable<Product | undefined> {
+      const product = this.products.find(product => product.id === productId);
+      return of(product);
+    }
+  }
+
+function updateProduct(updatedProduct: any, User: any) {
+    throw new Error('Function not implemented.');
 }
+
