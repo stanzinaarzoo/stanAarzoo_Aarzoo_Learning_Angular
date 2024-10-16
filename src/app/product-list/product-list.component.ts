641405d35de57e6ee648a42c754dbb75bb7b20da
Aarzoo <w0864444@myscc.ca>
2024-10-16 13:06:30 -0400

    Completed step 8

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Product} from "../Shared/models/product";
 import {NgForOf} from "@angular/common";
 import {ProductListItemComponent} from "../product-list-item/product-list-item.component";
@@ -14,9 +14,17 @@ import {ProductService} from "../services/product.service";
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
-
+export class ProductListComponent implements OnInit {
+products: Product[]=[]; // for the storage for future
   constructor(private productService: ProductService) {
+  }
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: (data: Product[]) => this.products = data,
+      error: (error: Error) => console.log("Error fetching" ,error),
+      complete: () => console.log("Data fetching complete!")
+    })
   }
 
 }
