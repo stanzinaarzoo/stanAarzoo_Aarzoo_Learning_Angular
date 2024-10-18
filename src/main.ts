3c043a2dd53db03d4490fa5d7f8075e98ad93065
matt <mhaug@stclaircollege.ca>
2024-10-18 15:12:59 -0400

    Changes By Matt

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {provideRouter, Routes} from "@angular/router";
-import {ProductListComponent} from "./app/product-list/product-list.component";
-import {ModifiedProductComponent} from "./app/modified-product/modified-product.component";
-import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
-
+import { provideRouter, Routes } from '@angular/router';
+import { ProductListComponent } from './app/product-list/product-list.component';
+import { ModifiedProductComponent } from './app/modified-product/modified-product.component';
+import { PageNotFoundComponent } from './app/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-
-  { path: '', redirectTo: '/products', pathMatch: 'full'},//default route
+  { path: '', redirectTo: '/products', pathMatch: 'full' }, // default route
   { path: 'products', component: ProductListComponent },
-  { path: 'modifiedProduct', component: ModifiedProductComponent},
-  {path: "**", component: PageNotFoundComponent},
-  ]
+  { path: 'modifiedProduct', component: ModifiedProductComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
+
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes)],
 });
