ca937d5ef2c10b56e941ebe3ddc915ad814689c0
Aarzoo <w0864444@myscc.ca>
2024-10-21 16:48:34 -0400

    review quick one

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GalleryListComponent } from './gallery-list/gallery-list.component';
+import { GalleryDetailComponent } from './gallery-detail/gallery-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: 'art', component: GalleryListComponent },
+  { path: 'art/:id', component: GalleryDetailComponent },
+  { path: '', redirectTo: '/art', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
